49b04b46f930f0570bb5102c116b5319833e25c1
buga747 <110128294+buga747@users.noreply.github.com>
2022-10-16 15:59:21 +0000

    Deploying to gh-pages from @ buga747/icecream-shop@990a62826f33cdf68e82d216b8804fc1f8d4c68d 🚀

---
+++ index.8903e368.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.8903e368.js.map
---
+++ index.ee5c5579.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.ee5c5579.js.map
