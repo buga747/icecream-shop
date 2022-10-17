aa4d1aef2ac0c7cc88aa6c30537caa5396346bd9
buga747 <110128294+buga747@users.noreply.github.com>
2022-10-17 17:41:13 +0000

    Deploying to gh-pages from @ buga747/icecream-shop@9b1d556baf658b038028df615a851337d6f2753e 🚀

---
+++ index.e1dd6b70.js
@@ -0,0 +1,2 @@
+var o=$("#button");$(window).scroll((function(){$(window).scrollTop()>300?o.addClass("show"):o.removeClass("show")})),o.on("click",(function(o){o.preventDefault(),$("html, body").animate({scrollTop:0},"300")}));
+//# sourceMappingURL=index.e1dd6b70.js.map
---
+++ index.fa244093.js
@@ -0,0 +1,2 @@
+!function(){var o=$("#button");$(window).scroll((function(){$(window).scrollTop()>300?o.addClass("show"):o.removeClass("show")})),o.on("click",(function(o){o.preventDefault(),$("html, body").animate({scrollTop:0},"300")}))}();
+//# sourceMappingURL=index.fa244093.js.map
