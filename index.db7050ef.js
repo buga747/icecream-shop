e3808dc51b45fc1a130b7dabcd8bfe914d4a6741
buga747 <110128294+buga747@users.noreply.github.com>
2022-10-18 21:05:40 +0000

    Deploying to gh-pages from @ buga747/icecream-shop@02e5d863761d9da78f64d174c3ca4068123da801 🚀

---
+++ index.db7050ef.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.getElementById("product-modal-2-open"),closeModalBtn:document.getElementById("product-modal-2-close"),modal:document.getElementById("product-modal-2")},t=document.getElementById("body");function d(){e.modal.classList.toggle("is-hidden")}function o(){t.classList.toggle("locked")}e.openModalBtn.addEventListener("click",d),e.closeModalBtn.addEventListener("click",d),e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.db7050ef.js.map
